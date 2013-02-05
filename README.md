e917b4092064194310fed52689d071191d3d75e1
Olivier Grisel <olivier.grisel@ensta.org>
2013-02-05 18:41:32 +0100

    README Driven Development

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # nuxeo-demo-deployer
 
-Python based tooling to deploy demo instances of Nuxeo marketplace packages built on Jenkins CI
+Python based tooling to deploy demo instances of Nuxeo marketplace packages built
+on Jenkins CI.
+
+# Install
+
+	pip install -r requirements.txt
+	pip install .
+
+# Usage
+
+	$ export AWS_ACCESS_KEY_ID=XXXXXXXXX
+	$ export AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXXXXXXX
+
+	$ python -m nxdd \
+	         --node-tag my_demo_box
+	         --application-name my_demo_app
+	         --region us-east-1
+	         --nuxeo-version 5.6.0
+	         --package my-marketplace-package-version.zip
+	         --database postgresql
+	         --user ubuntu
---
+++ setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2013 Nuxeo and contributors
+
+DESCRIPTION = """Python based tooling for deploying demo Nuxeo instances"""
+VERSION = '0.1.0'
+
+from numpy.distutils.core import setup
+
+setup(
+    name="nuxeo-demo-deployer",
+    maintainer="Nuxeo",
+    maintainer_email="contact@nuxeo.com",
+    description=DESCRIPTION,
+    license="BSD",
+    url="http://github.com/nuxeo/nuxeo-demo-deployer",
+    version=VERSION,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        ])
